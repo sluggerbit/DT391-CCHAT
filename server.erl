f852f71d8f7425553445471768a22f359aab551c
sluggerbit <70601563+sluggerbit@users.noreply.github.com>
2021-09-27 11:23:29 +0200

    la till pid som return value för start i server

--- server.erl
+++ server.erl
@@ -9,8 +9,8 @@ start(ServerAtom) ->
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    genserver:start(ServerAtom, 0, messagehandler()).
-
+    Pid = genserver:start(ServerAtom, 0, messagehandler()),
+    Pid.
 messagehandler() -> 
     receive Msg -> 
     io:format("received: ~p~n", [Msg]) end,
